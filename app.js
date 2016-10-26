865019478690c7b07b29d5e498901ed9bfcced1d
Mustafa DERYOL <mustafa.deryol@hotmail.com>
2016-10-26 20:55:51 +0300

    Template Edit
    Template Edit

--- app.js
+++ app.js
@@ -14,19 +14,40 @@ angular.module('chatApplication', ['btford.socket-io']).
         $rootScope.message = null;
         $rootScope.username = prompt("Please enter your name", "");
 
+        $rootScope.textAreaSendKeypressFunction = function(event){
+            if($scope.message.length>0 && event.which === 13)
+            {
+                this.pushMessageFunction();
+            }
+        };
+
+        $rootScope.dateFormat = function(getDate){
+            var d = new Date(getDate);
+            var dformat = [
+                    d.getDate(),
+                    d.getMonth()+1,
+                    d.getFullYear()].join('/')+' '+
+                [d.getHours(),
+                    d.getMinutes()].join(':');
+            return dformat;
+        };
+
         $scope.pushMessageFunction = function(){
             $rootScope.message = $scope.message;
 
-            // $rootScope.chatPanelMessages.push({'username':$rootScope.username,'message':$rootScope.message,'date': Date()});
-
             var data =  {'username':$rootScope.username,'message':$rootScope.message,'date': Date()};
-            socket.emit('gonder',data);
+            socket.emit('messege:send',data);
 
             $scope.message = "";
         };
 
-        socket.on('alici', function (data) {
+        socket.on('messege:get', function (data) {
             $scope.chatPanelMessages.push(data);
+
+            var ses = new Audio('assets/zil.mp3');
+            ses.play();
+
+            $("html, body").animate({ scrollTop: $('.chat')[0].scrollHeight }, "fast");
         });
 
         $rootScope.isMe = function(deger){
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection',function (socket) {
 
-    socket.on('gonder',function (data) {
-        io.emit('alici', data);
+    socket.on('messege:send',function (data) {
+        io.emit('messege:get', data);
     })
 
 });
